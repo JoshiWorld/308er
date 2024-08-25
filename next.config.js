19ff5da3a837a3fb6926b332f062eba87a683054
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-08-25 21:11:17 +0200

    fix: added image remotePattern

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'rotekurve.s3.eu-north-1.amazonaws.com',
+                pathname: '/**'
+            }
+        ]
+    },
 };
